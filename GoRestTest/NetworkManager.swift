e8d43dbb9cf3049bef3c5c1c7e998563d3737c11
Marvin <Marvinegoavilz@gmail.com>
2020-12-17 19:58:21 +0000

    mi avance, marvin

--- GoRestTest/NetworkManager.swift
+++ GoRestTest/NetworkManager.swift
@@ -32,31 +32,67 @@ protocol GoRestAPI {
 
 extension Network: GoRestAPI {
     
-    static let ACCESS_TOKEN = "*****"
+    static let ACCESS_TOKEN = "aba8acb8ac933ecb3f0d9863142b736e1b56a9fbc359581d5abf44284501bffc"
     
     //TODO
     func getUsers() {
-        <#code#>
+        URLSession.shared.dataTask(with: URL(string: "https://gorest.co.in/public-api/users")!) { data, urlResponse, error in
+            
+            if error != nil {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                guard let data = data else { return }
+                print(data)
+            }
+            
+        }.resume()
     }
     
     //TODO
     func postUser() {
-        <#code#>
+        var request = URLRequest(url: URL(string: "https://gorest.co.in/public-api/users")!)
+        
+        request.addValue("Bearer \(Network.ACCESS_TOKEN)", forHTTPHeaderField: "Authorization")
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: {'name':'marvin', 'gender':'male','email':'marvin.com'}  , options: [])
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        URLSession.shared.dataTask(with: request) { data, urlResponse, error in
+            
+            if error != nil {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                guard let data = data else { return }
+                print(data)
+            }
+            
+        }.resume()
     }
     
     //TODO
     func patchUser() {
-        <#code#>
+        
     }
     
     //TODO
     func putUser() {
-        <#code#>
+        
     }
     
     //TODO
     func deleteUser() {
-        <#code#>
+        
     }
     
 }
